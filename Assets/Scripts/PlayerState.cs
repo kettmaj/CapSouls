410cf1387f19e07e30f23decaa593e94fb3fda45
Unknown <kettmaj@ferris.edu>
2018-09-30 19:28:37 -0400

    Start of State design
    Started state design pattern, still needs detail

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour {
+    /// <summary>
+    /// a new type to hold all the different states the player can be in
+    /// </summary>
+    public enum CurrentState //playerstate.state
+    {
+        idle,
+        walking,
+        running,
+        dodging,
+        attacking
+    }
+    public CurrentState state;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(state);
+        //i really don't know here
+	}
+}
